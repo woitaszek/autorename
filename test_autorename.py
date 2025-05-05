ea2b4a8459ebfb03378ed930495160a8f6de9892
Matthew Woitaszek <matthew.woitaszek@gmail.com>
2025-05-04 20:50:04 -0700

    Add tests for synthetic directory trees

--- test_autorename.py
+++ test_autorename.py
@@ -2,6 +2,8 @@
 
 # Python imports
 import os
+import logging
+import datetime
 
 # Test imports
 import pytest
@@ -116,6 +118,106 @@ def test_generate_filename_rename_extension_jpg(mock_setup):
     new_name = autorename.generate_filename("/path/does/not/exist", "filename.jpeg")
     assert new_name == "2010-01-02-d41d8cd98f.jpg"
 
+
+# ----------------------------------------------------------------------
+# Test configuration system
+# ----------------------------------------------------------------------
+
+
+def test_config_file_invalid_secion(tmp_path):
+    """
+    Test a configuration file with an invalid section
+    """
+
+    # Create an empty "jpg" file in the test directory
+    jpg_file = tmp_path / "empty.jpg"
+    jpg_file.touch()
+
+    # Create the ".autorename.ini" file in the test directory
+    ini_file = tmp_path / ".autorename.ini"
+    with ini_file.open("w") as f:
+        f.write("[invalid]\nplaceholder = invalid\n")
+
+    # Check that the configuration file read raises an exception
+    with pytest.raises(Exception):
+        autorename.get_directory_config(tmp_path)
+
+
+def test_config_file_invalid_value(tmp_path):
+    """
+    Test a configuration file with an invalid value
+    """
+
+    # Create an empty "jpg" file in the test directory
+    jpg_file = tmp_path / "empty.jpg"
+    jpg_file.touch()
+
+    # Create the ".autorename.ini" file in the test directory
+    ini_file = tmp_path / ".autorename.ini"
+    with ini_file.open("w") as f:
+        f.write("[autorename]\nprefix_timestamp = invalid\n")
+
+    # Check that the configuration file read raises an exception
+    with pytest.raises(Exception):
+        autorename.get_directory_config(tmp_path)
+
+
+def test_config_file_invalid_key(tmp_path):
+    """
+    Test a configuration file with an invalid key
+    """
+
+    # Create an empty "jpg" file in the test directory
+    jpg_file = tmp_path / "empty.jpg"
+    jpg_file.touch()
+
+    # Create the ".autorename.ini" file in the test directory
+    ini_file = tmp_path / ".autorename.ini"
+    with ini_file.open("w") as f:
+        f.write("[autorename]\ninvalid_key = invalid\n")
+
+    # Check that the configuration file read raises an exception
+    with pytest.raises(Exception):
+        autorename.get_directory_config(tmp_path)
+
+
+def test_config_file_hierarchy(tmp_path):
+    """
+    Test a nontrivial configuration file hierarchy
+    """
+
+    # Create the ".autorename.ini" file in the test directory
+    ini_file = tmp_path / ".autorename.ini"
+    with ini_file.open("w") as f:
+        f.write("[autorename]\nprefix_timestamp = minute\n")
+
+    # Create a temporary subdirectory that contains a jpg file
+    subdir = tmp_path / "testdir"
+    subdir.mkdir(exist_ok=True)
+    jpg_file = subdir / "empty.jpg"
+    with jpg_file.open("w") as f:
+        f.write("X\n")  # hash: 253bcac7dd806bb7cf57dc19f71f2fa0
+
+    # Get the mtime of the jpg file and create the expected filename
+    mtime_seconds = os.stat(jpg_file).st_mtime
+    mtime_datetime = datetime.datetime.fromtimestamp(mtime_seconds)
+    expected_filename = mtime_datetime.strftime("%Y-%m-%d-%H%M-") + "253bcac7dd.jpg"
+    # Check that the configuration file is read when starting in the test directory
+    config = autorename.get_directory_config(tmp_path)
+    assert config["prefix_timestamp"] == "minute"
+
+    # Check that the configuration file is read when starting in a subdirectory
+    config = autorename.get_directory_config(subdir)
+    assert config["prefix_timestamp"] == "minute"
+
+    # Test renaming a file in the subdirectory
+    logging.info(f"Renaming file in {subdir}")
+    target_file = subdir / "empty.jpg"
+    logging.info(f"Renaming file {target_file}")
+    assert target_file.exists()
+
+    autorename.process_directory(subdir, dryrun=False)
+    assert os.path.exists(subdir / expected_filename)
 
 
 # ----------------------------------------------------------------------
